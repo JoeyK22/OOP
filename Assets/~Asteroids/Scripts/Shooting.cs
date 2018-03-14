4bbccdc39bc1bd7f1ebe7daf3dbbfaa43c0d70ef
jack.harrington9 <jack.harrington9@600900B211DE408>
2018-03-14 15:59:24 +1100

    added shooting

---
+++ Assets/~Asteroids/Scripts/Shooting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Asteroids
+{
+    public class Shooting : MonoBehaviour
+    {
+        public GameObject bulletPrefab;
+        public float bulletSpeed = 5f;
+        public Transform spawnPoint;
+
+        public void Fire(Vector3 direction)
+        {
+            GameObject clone = Instantiate(bulletPrefab);
+            // Set position of clone to spawn point
+            clone.transform.position = spawnPoint.position;
+            float angle = Mathf.Atan2(direction.y, direction.x);
+            float degrees = angle * Mathf.Rad2Deg;
+            Rigidbody2D rigid = clone.GetComponent<Rigidbody2D>();
+            rigid.rotation = degrees;
+
+
+            rigid.AddForce(direction * bulletSpeed, ForceMode2D.Impulse);
+
+        }
+    }
+}
