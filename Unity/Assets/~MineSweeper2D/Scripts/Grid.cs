7e8e8ebdcdb7ad24b8f8ef08e19ac74130b4a71b
jack.harrington9 <jack.harrington9@600900B211DE408>
2018-05-02 16:03:01 +1000

    made dog game

---
+++ Unity/Assets/~MineSweeper2D/Scripts/Grid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minesweeper2D
+{
+
+    public class Grid : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Visualstudio/ClassesAndObjects/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+
+
+    class Color
+    {
+        public float r, g, b;
+    }
+
+    class Dog
+    {
+        public string name;
+        public int size;
+        public string breed;
+        public ConsoleColor color;
+
+        public void Eat(string food)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(name + " is eating " + food);
+        }
+        public void Sleep()
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(name + " is sleeping");
+        }
+        public void Shit()
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(name + " is shitting");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Color red = new Color();
+            red.r = 1;
+            red.g = 0;
+            red.b = 0;
+
+            // Create instance of Dog
+            Dog dog1 = new Dog();
+            // Set properties of instance
+            dog1.name = "Lassie";
+            dog1.size = 2;
+            dog1.breed = "Dalmation";
+            dog1.color = ConsoleColor.Cyan;
+
+            dog1.Eat("Meat"); // Cos ya can't have any puddin if you don't eat ya meat!
+            dog1.Shit();
+
+            Console.ReadLine();
+        }
+    }
+}
+
